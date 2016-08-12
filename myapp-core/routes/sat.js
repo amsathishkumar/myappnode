46c45658822a93a7fd515f79299586a7ef3560d6
sathishkumar <amsathishkumar@gmail.com>
2016-08-12 17:43:14 +0530

    node android

--- myapp-core/routes/sat.js
+++ myapp-core/routes/sat.js
@@ -2,6 +2,8 @@ var express = require('express');
 var multer = require('multer');
 
 var router = express.Router();
+var done = false;
+var satjson;
 
 var sUpload = multer({
 	dest: __dirname,
@@ -12,6 +14,7 @@ var sUpload = multer({
         console.log("onFileUploadStart");      },
     onFileUploadComplete: function (file) {
     	 console.log("onFileUploadComplete");
+    	 done = true;
     },
     changeDest: function(dest, req, res) { 
     	console.log("changeDest");
@@ -23,12 +26,26 @@ var sUpload = multer({
 });
 
 function scomplete (req, res){
-	res.status(200).send("File upload to sat complete\n");
+	if (done === true){
+		res.status(200).send("File upload to sat complete\n");
+	}
+	else{
+		res.status(500).send("Internal error occurred during file upload\n");
+		}
 }
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('SAT demo');
+  //res.send('SAT demo');
+	//res.json([{"id": 1,"name":"sathishkumar","age":32,"mail":"satjson@gmail.com"}]);
+	res.json(satjson);
+});
+
+router.post('/info', function (req, res, next) {
+	console.log("started");
+	satjson=req.body;
+	console.log(satjson); 
+	
 });
 
 router.post('/', sUpload,scomplete);
