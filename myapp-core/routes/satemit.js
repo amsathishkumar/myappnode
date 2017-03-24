c8c8725f5ca956c69e4aebb67558dfd8d5ac29a7
sathishkumar <amsathishkumar@gmail.com>
2017-03-24 11:55:40 +0530

    mongo

--- myapp-core/routes/satemit.js
+++ myapp-core/routes/satemit.js
@@ -1,13 +1,15 @@
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
+var logger = require( './log4js-conf' ).logging.getLogger( 'satemit' );
+
 // listener #1
 var listner1 = function listner1() {
-   console.log('listner1 executed.');
+	logger.info('listner1 executed.');
 }
 
 // listener #2
 var listner2 = function listner2() {
-  console.log('listner2 executed.');
+  logger.info('listner2 executed.');
 }
 
 // Bind the connection event with the listner1 function
@@ -18,7 +20,7 @@ eventEmitter.on('connection', listner2);
 
 function satemitcomplete (){
 	    eventEmitter.emit('connection'),
-		console.log("sat emit completed....................");
+	    logger.info("sat emit completed....................");
 }
 
 exports.satemitcomplete = satemitcomplete;
